0f86576b1ba694d63f1df726db5741bddca6d02c
AnacondaKC <113591529+AnacondaKC@users.noreply.github.com>
2025-05-08 14:50:22 +0800

    Update diy-part3.sh

--- scripts/zn-m2/diy-part3.sh
+++ scripts/zn-m2/diy-part3.sh
@@ -21,6 +21,3 @@ git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/l
 
 # ttyd免登陆
 sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config
-
-# design修改proxy链接
-sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
